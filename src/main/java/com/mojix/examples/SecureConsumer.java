16490a35d9818f397f28e09f1c1933ca3d00b2a6
vramos <victor.ramos@mojix.com>
2020-03-11 09:27:53 -0400

    Dockerfile for the consumer

--- src/main/java/com/mojix/examples/SecureConsumer.java
+++ src/main/java/com/mojix/examples/SecureConsumer.java
@@ -12,30 +12,45 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
-import java.util.Scanner;
 
 public class SecureConsumer {
 
     private boolean run = true;
 
     private void start(){
+        String kafkaServers = System.getenv("KAFKA_SERVERS");
+        String kafkaTopic = System.getenv("KAFKA_TOPIC");
+        String kafkaConsumerGroup = System.getenv("KAFKA_CONSUMER_GROUP");
+
+        String saslUser = System.getenv("SASL_JAAS_USER");
+        String saslPassword = System.getenv("SASL_JAAS_PASSWORD");
+        String saslEnabledMechanisms = System.getenv("SASL_ENABLED_MECHANISMS");
+        String saslJaasConfig = "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"" + saslUser + "\" password=\"" + saslPassword + "\";";
+
+        String sslTruststoreLocation = System.getenv("SSL_TRUSTSTORE_LOCATION");
+        String sslTruststorePassword = System.getenv("SSL_TRUSTSTORE_PASSWORD");
+        String sslKeystoreLocation = System.getenv("SSL_KEYSTORE_LOCATION");
+        String sslKeystorePassword = System.getenv("SSL_KEYSTORE_PASSWORD");
+        String sslKeyPassword = System.getenv("SSL_KEY_PASSWORD");
+
         Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.100.0.111:9192");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "SecureKafkaTest");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaConsumerGroup);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
-        props.put(SaslConfigs.SASL_MECHANISM, "PLAIN");
-        props.put(SaslConfigs.SASL_JAAS_CONFIG, "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"farfetch\" password=\"farfetch-secret\";");
-        props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, getClass().getClassLoader().getResource("kafka.truststore.jks").getPath());
-        props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, "Control123!");
-        props.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, getClass().getClassLoader().getResource("kafka.keystore.jks").getPath());
-        props.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, "Control123!");
-        props.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, "Control123!");
+        props.put(SaslConfigs.SASL_MECHANISM, saslEnabledMechanisms);
+        props.put(SaslConfigs.SASL_JAAS_CONFIG, saslJaasConfig);
+        props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, sslTruststoreLocation);
+        props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, sslTruststorePassword);
+        props.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, sslKeystoreLocation);
+        props.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, sslKeystorePassword);
+        props.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, sslKeyPassword);
         props.put(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, "");
 
         KafkaConsumer consumer = new KafkaConsumer(props);
-        consumer.subscribe(Collections.singleton("test_topic"));
+        consumer.subscribe(Collections.singleton(kafkaTopic));
+        System.out.println("Reading from " + kafkaTopic);
 
         new Thread(() -> {
             try{
@@ -60,10 +75,5 @@ public class SecureConsumer {
         SecureConsumer app = new SecureConsumer();
         app.start();
         System.out.println();
-        System.out.println("Reading from test_topic");
-        System.out.println("Press [Enter] when you want to stop consuming");
-        Scanner sc = new Scanner(System.in);
-        sc.nextLine();
-        app.run = false;
     }
 }
